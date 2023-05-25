6ac69b55978a0c09ede0bf1e8a966079eb841361
Iva <ivasaata@gmail.com>
2023-05-25 11:43:26 +0400

    Added button and fixed styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import styled, { createGlobalStyle } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
+import StyledContainer from "./components/styles/Container.styled.js";
+import HomeButton from "./components/homeButton";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>Quiz</h1>
+      <StyledContainer>
+        <h1 style={{marginTop: ""}}>Quiz</h1>
+        <h4>Check your genereal knolwedge here</h4>
+        <HomeButton onClick={() => {}} />
+      </StyledContainer>
     </>
   );
 }
--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -6,7 +6,20 @@ const GlobalStyles = createGlobalStyle`
   background: #252525;
   font-size: 1.15em;
   font-family: Arial, Helvetica, sans-serif;
-  color: #FFFFFF
+  color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  }
+
+  h1{
+    font-size: 3em;
+  }
+
+  h4{
+    margin-bottom: 2em;
   }
 `;
 
